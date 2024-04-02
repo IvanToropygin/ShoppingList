55e92cd7261fefd6f269fbd06c5a9c824df66f5b
Ivan Toropygin <itori86@mail.ru>
2024-04-02 09:05:55 +0300

    add ShopItemActivity

---
+++ app/src/main/java/com/stepik/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,15 @@
+package com.stepik.shoppinglist.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.stepik.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
