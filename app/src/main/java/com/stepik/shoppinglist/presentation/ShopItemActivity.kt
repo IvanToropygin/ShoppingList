8f1a3f826a04adfddb565059a61fac3400f4337e
Ivan Toropygin <itori86@mail.ru>
2024-04-02 18:46:06 +0300

    fix delete unused imports

--- app/src/main/java/com/stepik/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/stepik/shoppinglist/presentation/ShopItemActivity.kt
@@ -5,11 +5,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
-import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
